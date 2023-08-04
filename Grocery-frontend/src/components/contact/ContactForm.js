f0b976f2240cda3b0a8fb2cc0a87650ff56bf58f
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2023-08-04 23:59:48 +0530

    create service, blog page

--- Grocery-frontend/src/components/contact/ContactForm.js
+++ Grocery-frontend/src/components/contact/ContactForm.js
@@ -1,19 +1,9 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import CardMedia from "@mui/material/CardMedia";
-import bgImage1 from "../../assets/images/blogs/CEO-Advantages.jpg";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import CardActions from "@mui/material/CardActions";
-import IconButton from "@mui/material/IconButton";
-import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
-import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-import {Icon} from "@iconify/react";
-import bgImage2 from "../../assets/images/blogs/Shutterstock_1361250623-2.jpg";
-import bgImage3 from "../../assets/images/blogs/office-manager-woman.jpg";
+import address from "../../assets/images/blogs/1053163.png";
+import mail from "../../assets/images/blogs/1057057.png";
+import mobile from "../../assets/images/blogs/1062231.png";
 import {Form, FormGroup, Input, Label} from "reactstrap";
 import Button from "@mui/material/Button";
 
@@ -21,124 +11,39 @@ const ContactForm=()=>{
 
     return(
         <div className='mb-5' style={{display:"flex",justifyContent:"center",flexDirection:"column",padding:21,boxShadow:'0 1px 10px rgba(75,90,130,.1)',borderRadius:'0.375rem'}}>
-            <h2 style={{fontSize:24,color: 'black',fontWeight:600,lineHeight:'1.75rem',marginBottom:30}}>Contact Us</h2>
             <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={6} lg={6} style={{display:"flex",justifyContent:'center'}}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={12} lg={12} style={{display:"flex",justifyContent:'center'}}>
+                                <div className="" style={{display:"flex",justifyContent:"center",alignItems:"center",boxShadow:'rgba(75, 90, 130, 0.1) 0px 1px 10px',width:350}}>
+                                    <div className="me-3 ms-2" >
+                                        <img src={address} alt='address' style={{width:75,position:'relative',left:0,right:0,margin:'auto',display:'block',paddingTop:7}}/>
+                                        <h4 className="category-title" style={{color:'black',textAlign:'center',}}>Address</h4>
+                                        <p style={{textAlign:'center',}}>730 Glenstone Ave 65802, Springfield, US</p>
+                                    </div>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} md={12} lg={12} style={{display:"flex",justifyContent:'center'}}>
+                                <div className="" style={{display:"flex",justifyContent:"center",alignItems:"center",boxShadow:'rgba(75, 90, 130, 0.1) 0px 1px 10px',width:350}}>
+                                    <div className="me-3 ms-2" >
+                                        <img src={mobile} alt='mobile' style={{width:75,position:'relative',left:0,right:0,margin:'auto',display:'block',paddingTop:7}}/>
+                                        <h4 className="category-title" style={{color:'black',textAlign:'center',}}>Mobile</h4>
+                                        <p style={{textAlign:'center',}}>+123 123 654</p>
+                                    </div>
+                                </div>
+                            </Grid>
+                            <Grid item xs={12} md={12} lg={12} style={{display:"flex",justifyContent:'center'}}>
+                                <div className="" style={{display:"flex",justifyContent:"center",alignItems:"center",boxShadow:'rgba(75, 90, 130, 0.1) 0px 1px 10px',width:350}}>
+                                    <div className="me-3 ms-2" >
+                                        <img src={mail} alt='mail' style={{width:75,position:'relative',left:0,right:0,margin:'auto',display:'block',paddingTop:7}}/>
+                                        <h4 className="category-title" style={{color:'black',textAlign:'center',}}>Mail</h4>
+                                        <p style={{textAlign:'center',}}>contact@gmail.com</p>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </Grid>
 
-                        <Box sx={{ maxWidth: { xs: 320, sm: 470, }, bgcolor: 'background.paper' }}>
-                            <Tabs
-                                variant="scrollable"
-                                scrollButtons="auto"
-                                aria-label="scrollable auto tabs example"
-                            >
-                                <Tab label={<div style={{boxShadow:'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px'}} >
-                                    <CardMedia
-                                        component="img"
-                                        height="194"
-                                        image={bgImage1}
-                                        alt="ceo"
-                                    />
-                                    <CardContent>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12} md={12} lg={12} >
-                                                <Typography variant="button" display="block" gutterBottom color="text" style={{display:'flex',justifyContent:'center'}}>
-                                                    CEO
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    Mr. Daniel Jonson
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    +94 751882696
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </CardContent>
-                                    <CardActions style={{display:'flex',justifyContent:'center'}}>
-                                        <IconButton aria-label="email">
-                                            <AlternateEmailIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="facebook">
-                                            <FacebookOutlinedIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="twitter">
-                                            <Icon icon="entypo-social:linkedin" />
-                                        </IconButton>
-                                    </CardActions>
-
-                                </div>} />
-                                <Tab label={<div style={{boxShadow:'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px'}} >
-                                    <CardMedia
-                                        component="img"
-                                        height="194"
-                                        image={bgImage2}
-                                        alt="Paella dish"
-                                    />
-                                    <CardContent>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12} md={12} lg={12} >
-                                                <Typography variant="button" display="block" gutterBottom color="text" style={{display:'flex',justifyContent:'center'}}>
-                                                    Director
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    Mr. Justin Vendor
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    +94 775886214
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </CardContent>
-                                    <CardActions style={{display:'flex',justifyContent:'center'}}>
-                                        <IconButton aria-label="email">
-                                            <AlternateEmailIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="facebook">
-                                            <FacebookOutlinedIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="twitter">
-                                            <Icon icon="entypo-social:linkedin" />
-                                        </IconButton>
-                                    </CardActions>
-
-                                </div>} />
-                                <Tab label={<div style={{boxShadow:'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px'}} >
-                                    <CardMedia
-                                        component="img"
-                                        height="194"
-                                        image={bgImage3}
-                                        alt="manager"
-                                    />
-                                    <CardContent>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12} md={12} lg={12} >
-                                                <Typography variant="button" display="block" gutterBottom color="text" style={{display:'flex',justifyContent:'center'}}>
-                                                    Manager
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    Mrs. Mary Ann
-                                                </Typography>
-                                                <Typography variant="body2" color="text.secondary" style={{display:'flex',justifyContent:'center'}}>
-                                                    +94 7125589647
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </CardContent>
-                                    <CardActions style={{display:'flex',justifyContent:'center'}}>
-                                        <IconButton aria-label="email">
-                                            <AlternateEmailIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="facebook">
-                                            <FacebookOutlinedIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="twitter">
-                                            <Icon icon="entypo-social:linkedin" />
-                                        </IconButton>
-                                    </CardActions>
-
-                                </div>} />
-                            </Tabs>
-                        </Box>
                     </Grid>
 
                     <Grid item xs={12} md={6} lg={6}>
@@ -164,7 +69,6 @@ const ContactForm=()=>{
                     </Grid>
                 </Grid>
             </Box>
-            <br/>
         </div>
     )
 }
